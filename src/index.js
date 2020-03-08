c12179e7e5f5f92be4e425be7008cdcfbcdba3ad
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-08 18:40:54 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+	if (Math.sign(n) === -1) {
+		n = n * -1;
+	}
+	let array = ('' + n).split('');
+	let arrayReverse = new Array(array.length);
+	let j = array.length;
+	for (let i = 0; i < array.length; i++) {
+		arrayReverse[j] = array[i];
+		j--;
+	}
+	return +arrayReverse.join('');
+};
